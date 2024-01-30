2a8e950a820c29f1fed263e03a282554abf561c9
VividVibez <102436897+VividVibez@users.noreply.github.com>
2024-01-30 10:02:51 +0000

    Added method for catching errors

--- database.php
+++ database.php
@@ -1,17 +1,31 @@
 <?php
 
+// Connection details
+
 $host = "localhost";
 $dbname = "login_db";
 $username = "root";
 $password = "";
 
-$mysqli = new mysqli(hostname: $host,
-                     username: $username,
-                     password: $password,
-                     database: $dbname);
 
-if ($mysqli->connect_errno) {
-    die("Connection error: " . $mysqli->connect_error);
+// Create connection
+
+try {
+    $mysqli = new mysqli(hostname: $host,
+                username: $username,
+                password: $password,
+                database: $dbname);
 }
+
+// Check for connection errors
+
+catch (Exception $e) {
+    if ($e->getMessage() == mysqli_connect_error()) {
+        // Print error message
+        die($e->getMessage());
+    }
+}
+
+// Return database to signup.php
 
 return $mysqli;
