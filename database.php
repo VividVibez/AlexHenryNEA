f402b0a7d85b56cef1b26247ed6ca690ca629707
VividVibez <102436897+VividVibez@users.noreply.github.com>
2024-01-31 08:44:52 +0000

    Finished database.php

--- database.php
+++ database.php
@@ -4,24 +4,27 @@
 
 $host = "localhost";
 $dbname = "login_db";
-$username = "root";
-$password = "";
+$user = "root";
+$pword = "";
 
 
 // Create connection
 
 try {
     $mysqli = new mysqli(hostname: $host,
-                username: $username,
-                password: $password,
+                username: $user,
+                password: $pword,
                 database: $dbname);
 }
 
 // Check for connection errors
 
 catch (Exception $e) {
+
     if ($e->getMessage() == mysqli_connect_error()) {
+
         // Print error message
+
         die($e->getMessage());
     }
 }
