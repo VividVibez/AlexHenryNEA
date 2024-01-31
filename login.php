d24687902af1af396410c74d627ea1bc028ee3c8
VividVibez <102436897+VividVibez@users.noreply.github.com>
2024-01-31 12:35:11 +0000

    Fixed logic error

--- login.php
+++ login.php
@@ -11,16 +11,17 @@ $password = $_POST["loginPassword"];
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
 
-// Setup sql to check if username and password are in the database
-
-$check = "SELECT password_hash
-          FROM user
-          WHERE username=$username";
-
-
 // Check if user exists function
 
 function userVerify($pw, $un) {
+
+
+    // Setup sql to check if username and password are in the database
+
+    $check = "SELECT password_hash
+    FROM user
+    WHERE username=$username";
+
 
     // Connect to database by calling 'database.php'
 
