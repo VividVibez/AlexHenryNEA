2908eb7e5de6f3cfaadcca0d5a7cfd14e47b6f56
VividVibez <102436897+VividVibez@users.noreply.github.com>
2024-01-31 12:33:01 +0000

    Function to check if user exists

--- login.php
+++ login.php
@@ -18,3 +18,28 @@ $check = "SELECT password_hash
           WHERE username=$username";
 
 
+// Check if user exists function
+
+function userVerify($pw, $un) {
+
+    // Connect to database by calling 'database.php'
+
+    $conn =  require __DIR__ . "/database.php";
+
+    
+    // Check user exists
+
+    $rs = mysqli_query($conn,$check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
+
+    if ($data[0] != 0) {
+
+        // User exists
+        return TRUE;
+
+    } else {
+        //User doesn't exist
+        return FALSE;
+    }
+
+}
