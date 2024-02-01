eb17404c83358465f3a1f9d12c0856112f70f3a9
VividVibez <102436897+VividVibez@users.noreply.github.com>
2024-02-01 12:12:19 +0000

    Update main.php

--- Main/main.php
+++ Main/main.php
@@ -1,1 +1,22 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account Setup</title>
+</head>
+<body>
+    <div class="wrapper">
+        <h1>Account Setup</h1>
+        <p>Please answer the following questions to help design a plan for you.</p>
+        <form method="post">
+            <div class="q1">
+                <h3>What is your training experience?</h3>
+                <input type="range" class="slider" id="range" min="0" max="10">
+                <div class="value">20</div>
+                <button></button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
