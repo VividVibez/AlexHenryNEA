ddb65b86350acbd74ce5156f21ccbc8d8292c55f
VividVibez <102436897+VividVibez@users.noreply.github.com>
2024-02-06 18:10:04 +0000

    Update functions.php
    Function to save answers to database

--- functions.php
+++ functions.php
@@ -1,1 +1,56 @@
 <?php
+function saveQuestion($usr, $loc, $val) {
+
+    $conn = require __DIR__ . "/database.php";
+
+    $setup = "INSERT INTO basic_info (username)
+    VALUE (?)";
+
+    $check = "SELECT COUNT(*) 
+    FROM basic_info 
+    WHERE username = '$usr'";
+
+    $rs = mysqli_query($conn, $check);
+    $data = mysqli_fetch_array($rs, MYSQLI_NUM);
+
+    function save($conn, $usr, $loc, $val) {
+
+        $save = "UPDATE basic_info
+        SET $loc=$val
+        WHERE username = '$usr'";
+
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt, $save)) {
+            die(mysqli_error($conn)); // Terminate script execution and display the error message if preparation fails
+        }
+
+        //mysqli_stmt_bind_param($stmt, "i", $val);
+
+        // Execute the statement to store the username and hashed password in the user table
+        mysqli_stmt_execute($stmt);     
+        
+    }
+    
+    if ($data[0] >= 1) {
+
+        save($conn, $usr, $loc, $val);
+
+    } else {
+
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt, $setup)) {
+            die(mysqli_error($conn)); // Terminate script execution and display the error message if preparation fails
+        }
+
+        mysqli_stmt_bind_param($stmt, "s", $usr);
+
+        // Execute the statement to store the username and hashed password in the user table
+        mysqli_stmt_execute($stmt);
+
+        save($conn, $usr, $loc, $val);
+        
+    }
+}
+?>
