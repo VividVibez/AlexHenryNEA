2fd945835fa12b459533906d948c2831ade8bddc
VividVibez <102436897+VividVibez@users.noreply.github.com>
2024-03-25 08:55:04 +0000

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,9 @@
 <?php
+// Start the session to access session variables
 session_start();
+// Unset the session variable named "usr"
 unset($_SESSION["usr"]);
+// Redirect the user to the login.php page
 header("location: login.php");
+?>
 ?>
